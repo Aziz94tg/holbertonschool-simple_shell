243dbee8a61013aaa8adeebd15f41ef2df6a8501
Aziz94tg <aziz94tg@gmail.com>
2025-04-29 15:54:13 +0000

    A

--- main.c
+++ main.c
@@ -1,118 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
+#define MAX_TOKENS 64
 #define DELIM " \t\r\n\a"
 
-char *_getenv(const char *name)
+char **parse_line(char *line)
 {
-	int i = 0, len = strlen(name);
+    char **tokens = malloc(MAX_TOKENS * sizeof(char *));
+    char *token;
+    int position = 0;
 
-	while (environ[i])
-	{
-		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-			return &environ[i][len + 1];
-		i++;
-	}
-	return NULL;
-}
+    if (!tokens)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
-char *find_command_path(char *command)
-{
-	char *path = _getenv("PATH");
-	char *token, *full_path;
-	size_t cmd_len = strlen(command);
-
-	if (!path)
-		return NULL;
-
-	path = strdup(path);
-	token = strtok(path, ":");
-
-	while (token)
-	{
-		full_path = malloc(strlen(token) + cmd_len + 2);
-		if (!full_path)
-		{
-			free(path);
-			return NULL;
-		}
-		strcpy(full_path, token);
-		strcat(full_path, "/");
-		strcat(full_path, command);
-
-		if (access(full_path, X_OK) == 0)
-		{
-			free(path);
-			return full_path;
-		}
-		free(full_path);
-		token = strtok(NULL, ":");
-	}
-	free(path);
-	return NULL;
+    token = strtok(line, DELIM);
+    while (token != NULL && position < MAX_TOKENS - 1)
+    {
+        tokens[position++] = token;
+        token = strtok(NULL, DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
 }
 
 int main(int argc, char **argv)
 {
-	char *line = NULL, *command_path = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	int line_number = 0;
-	char *args[2];
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int line_number = 0;
+    char **args;
 
-	(void)argc;
+    (void)argc;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, ":) ", 3);
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, ":) ", 3);
 
-		nread = getline(&line, &len, stdin);
-		if (nread == -1)
-			break;
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+            break;
 
-		line_number++;
-		line[nread - 1] = '\0'; /* remove newline */
+        line_number++;
+        if (line[read - 1] == '\n')
+            line[read - 1] = '\0';
 
-		if (line[0] == '\0')
-			continue;
+        if (line[0] == '\0')
+            continue;
 
-		if (access(line, X_OK) == 0)
-		{
-			command_path = strdup(line);
-		}
-		else
-		{
-			command_path = find_command_path(line);
-		}
+        args = parse_line(line);
 
-		if (!command_path)
-		{
-			fprintf(stderr, "%s: %d: %s: not found\n", argv[0], line_number, line);
-			continue;
-		}
+        if (access(args[0], X_OK) != 0)
+        {
+            fprintf(stderr, "%s: %d: %s: not found\n", argv[0], line_number, args[0]);
+            free(args);
+            continue;
+        }
 
-		args[0] = command_path;
-		args[1] = NULL;
+        if (fork() == 0)
+        {
+            execve(args[0], args, environ);
+            perror("execve");
+            exit(127);
+        }
+        else
+        {
+            wait(NULL);
+            free(args);
+        }
+    }
 
-		if (fork() == 0)
-		{
-			execve(command_path, args, environ);
-			perror("execve");
-			exit(1);
-		}
-		else
-		{
-			wait(NULL);
-			free(command_path);
-		}
-	}
-	free(line);
-	return 0;
+    free(line);
+    return 0;
 }
 
