7d359906366610599a078181d7557eb123c11f14
Aziz94tg <aziz94tg@gmail.com>
2025-04-29 14:49:39 +0000

    A

--- main.c
+++ main.c
@@ -4,91 +4,94 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define MAX_ARGS 64
+
+extern char **environ;
 
 /**
- * is_whitspace - check for spaces
- * @str - strnig to check 
- * Return: 0 if not or 1 if all whitespace
+ * is_whitespace - checks if a string is all whitespace
+ * @str: input string
+ * Return: 1 if all whitespace, 0 otherwise
  */
 int is_whitespace(const char *str)
 {
 	while (*str)
 	{
 		if (*str != ' ' && *str != '\t' && *str != '\n' &&
-			*str != '\r' && *str != '\v' && *str != '\f')
+		    *str != '\r' && *str != '\v' && *str != '\f')
 			return (0);
 		str++;
 	}
 	return (1);
 }
 
-extern char **environ;
-
 /**
  * main - Simple shell 0.1
- *
  * Return: Always 0
  */
-int main(__attribute__((unused)) int ac, char **argv)
+int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    pid_t child_pid;
+	char *line = NULL, *token = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t child_pid;
+	char *cmd_argv[MAX_ARGS];
+	int i;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO)) /* Only print prompt in interactive mode */
-            printf("#cisfun$ ");
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("#cisfun$ ");
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1) /* EOF (Ctrl+D) or error */
-        {
-            free(line);
-	    line = NULL;
-            if (isatty(STDIN_FILENO))
-                printf("\n");
-            break;
-        }
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
+		}
 
-        /* Remove newline */
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
-        if (line[0] == '\0' || is_whitespace(line)) /* Empty input */
-            continue;
+		if (line[0] == '\0' || is_whitespace(line))
+			continue;
 
-        child_pid = fork();
-        if (child_pid == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("fork");
+			free(line);
+			exit(EXIT_FAILURE);
+		}
 
-        if (child_pid == 0)
-        {
-            char *cmd_argv[2];
+		if (child_pid == 0)
+		{
+			i = 0;
+			token = strtok(line, " \t\r\n");
+			while (token && i < MAX_ARGS - 1)
+			{
+				cmd_argv[i++] = token;
+				token = strtok(NULL, " \t\r\n");
+			}
+			cmd_argv[i] = NULL;
 
-	    cmd_argv[0] = strtok(line, " \t\r\n");
-	    
-	    if(cmd_argv[0] == NULL)
-		    exit(EXIT_SUCCESS);
+			if (cmd_argv[0] == NULL)
+				exit(EXIT_SUCCESS);
 
-	    cmd_argv[1] = NULL;
+			if (execve(cmd_argv[0], cmd_argv, environ) == -1)
+			{
+				perror(cmd_argv[0]);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
 
-	    if (execve(cmd_argv[0], cmd_argv, environ) == -1)
-            {
-                perror(argv[0]); /* as required in error format */
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(NULL); /* Parent waits for child */
-        }
-    }
-
-    free(line);
-    return (0);
+	free(line);
+	return (0);
 }
 
